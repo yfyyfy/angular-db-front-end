1b4ce8a78c357bdf50c47f37af5b39c1d885aaf0
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2017-11-25 19:46:16 +0900

    Restructure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DetailComponent } from './detail/detail.component';
+import { MessagesComponent } from './messages/messages.component';
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/search', pathMatch: 'full' },
   { path: 'detail/:mode', component: DetailComponent },
   { path: 'detail/:mode/:id', component: DetailComponent },
+  { path: 'messages', component: MessagesComponent },
   { path: 'search', component: SearchComponent }
 ];
 
