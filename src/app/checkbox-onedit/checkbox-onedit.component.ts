f1d0f78612487bde97b80aeaac3b56470d82a89d
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2017-11-26 23:43:12 +0900

    Allow *-or-span component to take ngModel
    cf. https://embed.plnkr.co/nqKUSPWb6w5QXr8a0wEu/?show=preview

--- src/app/checkbox-onedit/checkbox-onedit.component.ts
+++ src/app/checkbox-onedit/checkbox-onedit.component.ts
@@ -1,22 +1,60 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+const noop = () => {
+};
+
+export const CHECKBOX_ONEDIT_COMPONENT_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => CheckboxOneditComponent),
+  multi: true
+};
 
 @Component({
   selector: 'app-checkbox-onedit',
   templateUrl: './checkbox-onedit.component.html',
-  styleUrls: ['./checkbox-onedit.component.css']
+  styleUrls: ['./checkbox-onedit.component.css'],
+  providers: [CHECKBOX_ONEDIT_COMPONENT_ACCESSOR]
 })
-export class CheckboxOneditComponent implements OnInit {
-  @Input() model: any;
+export class CheckboxOneditComponent implements ControlValueAccessor {
   @Input() label: string;
-  @Input() property: string;
   @Input() editMode: boolean;
   @Input() checkboxString: string;
   @Input() checkedString: string;
   @Input() unCheckedString: string;
 
-  constructor() { }
+  // The internal data model
+  private innerValue: any = '';
 
-  ngOnInit() {
+  // Placeholders for the callbacks which are later provided
+  // by the Control Value Accessor
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
+  // get/set accessor
+  get value(): any {
+    return this.innerValue;
+  };
+
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
   }
 
+  // ControlValueAccessor interfaces
+  writeValue(value: any) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
 }
--- src/app/input-onedit/input-onedit.component.ts
+++ src/app/input-onedit/input-onedit.component.ts
@@ -1,21 +1,59 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+const noop = () => {
+};
+
+export const INPUT_ONEDIT_COMPONENT_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => InputOneditComponent),
+  multi: true
+};
 
 @Component({
   selector: 'app-input-onedit',
   templateUrl: './input-onedit.component.html',
-  styleUrls: ['./input-onedit.component.css']
+  styleUrls: ['./input-onedit.component.css'],
+  providers: [INPUT_ONEDIT_COMPONENT_ACCESSOR]
 })
-export class InputOneditComponent implements OnInit {
-  @Input() model: any;
-  @Input() label: string;
-  @Input() property: string;
-  @Input() placeholder: string;
+export class InputOneditComponent implements ControlValueAccessor {
   @Input() editMode: boolean;
+  @Input() label: string;
+  @Input() placeholder: string;
   @Input() source: any[];
 
-  constructor() { }
+  // The internal data model
+  private innerValue: any = '';
 
-  ngOnInit() {
+  // Placeholders for the callbacks which are later provided
+  // by the Control Value Accessor
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
+  // get/set accessor
+  get value(): any {
+    return this.innerValue;
+  };
+
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
   }
 
+  // ControlValueAccessor interfaces
+  writeValue(value: any) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
 }
