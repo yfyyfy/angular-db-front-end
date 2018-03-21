72cf8428abef1a101e7152e6d6b577271917aa39
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2018-03-22 00:54:51 +0900

    Import SharedModule instead of each class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 
 import { MultiselectDropdownModule } from 'ng2-multiselect';
-import { NguiAutoCompleteModule }    from '@ngui/auto-complete';
+
+import { SharedModule }              from 'app/shared/shared.module';
 
 import { AppComponent }              from 'app/app.component';
-import { CheckboxOneditComponent }   from 'app/shared/checkbox-onedit/checkbox-onedit.component';
 import { DetailComponent }           from 'app/detail/detail.component';
-import { InputOneditComponent }      from 'app/shared/input-onedit/input-onedit.component';
 import { SearchResultsComponent }    from 'app/search-results/search-results.component';
 import { SearchFormComponent }       from 'app/search-form/search-form.component';
-import { MultiCheckboxComponent }    from 'app/shared/multi-checkbox/multi-checkbox.component';
-import { MultiInputOneditComponent } from 'app/shared/multi-input-onedit/multi-input-onedit.component';
 import { MessagesComponent }         from 'app/messages/messages.component';
 import { SearchComponent }           from 'app/search/search.component';
-
-import { PipesModule }            from 'app/shared/pipes/pipes.module';
 
 import { HeroService }               from 'app/services/hero.service';
 import { MessageService }            from 'app/services/message.service';
@@ -32,18 +27,13 @@ import { CustomReuseStrategy } from 'app/shared/custom-reuse-strategy';
     BrowserModule,
     FormsModule,
     MultiselectDropdownModule,
-    NguiAutoCompleteModule,
     AppRoutingModule,
-    PipesModule
+    SharedModule,
   ],
   declarations: [
     AppComponent,
-    CheckboxOneditComponent,
     DetailComponent,
-    InputOneditComponent,
     MessagesComponent,
-    MultiCheckboxComponent,
-    MultiInputOneditComponent,
     SearchComponent,
     SearchFormComponent,
     SearchResultsComponent,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NguiAutoCompleteModule } from '@ngui/auto-complete';
+
+import { PipesModule }               from './pipes/pipes.module';
+
+import { CheckboxOneditComponent }   from './checkbox-onedit/checkbox-onedit.component';
+import { InputOneditComponent }      from './input-onedit/input-onedit.component';
+import { MultiCheckboxComponent }    from './multi-checkbox/multi-checkbox.component';
+import { MultiInputOneditComponent } from './multi-input-onedit/multi-input-onedit.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    NguiAutoCompleteModule,
+    PipesModule
   ],
-  declarations: []
+  declarations: [
+    CheckboxOneditComponent,
+    InputOneditComponent,
+    MultiCheckboxComponent,
+    MultiInputOneditComponent,
+  ],
+  exports: [
+    CheckboxOneditComponent,
+    InputOneditComponent,
+    MultiCheckboxComponent,
+    MultiInputOneditComponent,
+    CommonModule,
+    FormsModule,
+    PipesModule,
+  ],
 })
 export class SharedModule { }
